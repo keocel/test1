9c73f590204773cf056e83082eb2191f74250e21
keocel <39299216+keocel@users.noreply.github.com>
2019-03-26 10:22:46 +0300

    hey

--- prog.go
+++ prog.go
@@ -1,4 +1,30 @@
 package main
-import "fmt"
-func main(){
+
+import (
+	"fmt"
+	"sort"
+)
+
+func alphsort(strinput string) string {
+	/*
+	   функция, сортирующая символы
+	   в строке в лексикографическом порядке
+	*/
+
+	var result string
+	symlist := make([]string, len(strinput))
+	for index, symbol := range strinput {
+		symlist[index] = string(symbol)
+	}
+	sort.Strings(symlist)
+	for _, symbol := range symlist {
+		result += symbol
+	}
+	return result
+}
+
+func main() {
+	fmt.Scan()
+	words := make([]string)
+	fmt.Println(alphsort(resultu))
 }
